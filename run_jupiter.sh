2090303eac5778ec571b33a7a6f853dfe38fa568
Cheng Li <cli@dungeon3>
2026-05-18 13:59:17 -0400

    wip

--- run_jupiter.sh
+++ run_jupiter.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-DEVICE_ID=1 python run_jupiter_dry.py -c jupiter_dry.yaml --output-dir ./output_260513_dry > log.260513_dry &
+#DEVICE_ID=1 python run_jupiter_dry.py -c jupiter_dry.yaml --output-dir ./output_260513_dry > log.260513_dry &
+#DEVICE_ID=0 python run_jupiter_dry.py -c jupiter_dry_7.5.yaml --output-dir ./output_260513_dry_7.5 > log.260513_dry_7.4 &
+DEVICE_ID=1 python run_jupiter_dry.py -c jupiter_dry_1000.yaml --output-dir ./output_260513_dry_1000 > log.260513_dry_1000 &
