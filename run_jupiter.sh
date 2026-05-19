c7f8e388b34de471e47a03c8e0e62105de8c215c
Cheng Li <chengcli@dart9>
2026-05-19 12:24:20 -0400

    Add H2O-NH3 system

--- run_jupiter.sh
+++ run_jupiter.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
-# moist 2D
-python -u run_jupiter_crm.py -c jupiter_crm2d_7.5.yaml --output-dir ./260518_crm2d_7.5 &> log.260518_crm2d_7.5 &
+# H2O-NH3 moist
+#DEVICE_ID=0 python -u run_jupiter_crm.py -c jupiter_crm2d_H2O-NH3_7.5.yaml --output-dir ./260518_crm2d_H2O-NH3_7.5 &> log.260518_crm2d_H2O-NH3_7.5 &
+DEVICE_ID=1 python -u run_jupiter_crm.py -c jupiter_crm3d_H2O-NH3_7.5.yaml --output-dir ./260518_crm3d_H2O-NH3_7.5 &> log.260518_crm3d_H2O-NH3_7.5 &
 
 # moist 3D
 #DEVICE_ID=0 python run_jupiter_crm.py -c jupiter_crm_7.5.yaml --output-dir ./output_260518_moist_7.5 > log.260518_moist_7.5 &
