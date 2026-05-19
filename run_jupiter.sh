d9fe75bfdb0ba4f6e1bd8ec4f32bf47df180e7be
Cheng Li <chengcli@umich.edu>
2026-05-19 11:56:47 -0400

    add 2d cases

--- run_jupiter.sh
+++ run_jupiter.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
-# moist
-DEVICE_ID=0 python run_jupiter_crm.py -c jupiter_crm_7.5.yaml --output-dir ./output_260518_moist_7.5 > log.260518_moist_7.5 &
-DEVICE_ID=1 python run_jupiter_crm.py -c jupiter_crm_1000.yaml --output-dir ./output_260518_moist_1000 > log.260518_moist_1000 &
+# moist 2D
+python -u run_jupiter_crm.py -c jupiter_crm2d_7.5.yaml --output-dir ./260518_crm2d_7.5 &> log.260518_crm2d_7.5 &
+
+# moist 3D
+#DEVICE_ID=0 python run_jupiter_crm.py -c jupiter_crm_7.5.yaml --output-dir ./output_260518_moist_7.5 > log.260518_moist_7.5 &
+#DEVICE_ID=1 python run_jupiter_crm.py -c jupiter_crm_1000.yaml --output-dir ./output_260518_moist_1000 > log.260518_moist_1000 &
 
 # dry
 #DEVICE_ID=1 python run_jupiter_dry.py -c jupiter_dry.yaml --output-dir ./output_260513_dry > log.260513_dry &
