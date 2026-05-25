048e41789b7ea9f56f94b719d5538f4823bdd19b
dungeon2/cli <chengcli@umich.edu>
2026-05-24 20:16:02 -0400

    update

--- run_jupiter.sh
+++ run_jupiter.sh
@@ -15,13 +15,13 @@
 
 # H2O-NH3-H2S moist + dry
 # dungeon1
-#DEVICE_ID=0 python -u run_jupiter_crm.py -c jupiter_crm_7.5.yaml --output-dir ./output_260520_crm_7.5 &> log.260520_crm_7.5 &
-#DEVICE_ID=1 python -u run_jupiter_dry.py -c jupiter_dry_7.5.yaml --output-dir ./output_260520_dry_7.5 &> log.260520_dry_7.5 &
+#DEVICE_ID=0 python -u run_jupiter_crm.py -c jupiter_crm_7.5.yaml --output-dir ./260520_crm_7.5 > log.260518_crm_7.5 &
+#DEVICE_ID=1 python -u run_jupiter_dry.py -c jupiter_dry_7.5.yaml --output-dir ./260520_dry_7.5 > log.260518_dry_7.5 &
 
 # dungeon2
-#DEVICE_ID=0 python -u run_jupiter_crm.py -c jupiter_crm_100.yaml --output-dir ./output_260520_crm_100 &> log.260520_crm_100 &
-#DEVICE_ID=1 python -u run_jupiter_dry.py -c jupiter_dry_100.yaml --output-dir ./output_260520_dry_100 &> log.260520_dry_100 &
+#DEVICE_ID=0 python -u run_jupiter_dry.py -c jupiter_dry_100.yaml --output-dir ./260520_dry_100 &> log.260520_dry_100 &
+#DEVICE_ID=1 python -u run_jupiter_crm.py -c jupiter_crm_100.yaml --output-dir ./260520_crm_100 &> log.260520_crm_100 &
 
 # dungeon3
-DEVICE_ID=0 python -u run_jupiter_crm.py -c jupiter_crm_1000.yaml --output-dir ./output_260520_crm_1000 &> log.260520_crm_1000 &
-DEVICE_ID=1 python -u run_jupiter_dry.py -c jupiter_dry_1000.yaml --output-dir ./output_260520_dry_1000 &> log.260520_dry_1000 &
+#DEVICE_ID=0 python run_jupiter_crm.py -c jupiter_crm_1000.yaml --output-dir ./260520_crm_1000 > log.260513_crm_1000 &
+#DEVICE_ID=1 python run_jupiter_dry.py -c jupiter_dry_1000.yaml --output-dir ./260520_dry_1000 > log.260513_dry_1000 &
