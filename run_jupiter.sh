23b453c7bc6da4a090a088564bf8ac9faef79d7e
dungeon2/cli <chengcli@umich.edu>
2026-06-02 14:38:13 -0400

    add case

--- run_jupiter.sh
+++ run_jupiter.sh
@@ -17,13 +17,18 @@
 # dungeon1
 #DEVICE_ID=0 python -u run_jupiter_crm.py -r jupiter_crm_7.5.final.restart -c jupiter_crm_7.5.yaml --output-dir ./260520_crm_7.5 &> log.260520_crm_7.5 &
 #DEVICE_ID=1 python -u run_jupiter_dry.py -r jupiter_dry_7.5.final.restart -c jupiter_dry_7.5.yaml --output-dir ./260520_dry_7.5 &> log.260520_dry_7.5 &
-torchrun --nproc-per-node 2 run_jupiter_moist.py -c jupiter_gcm_H2O-NH3-H2S_1000.yaml --output-dir ./260528_gcm_1000 &> log.260528_gcm_1000 &
+#torchrun --nproc-per-node 2 run_jupiter_moist.py -c jupiter_gcm_H2O-NH3-H2S_1000.yaml --output-dir ./260528_gcm_1000 &> log.260528_gcm_1000 &
+torchrun --nproc-per-node 2 run_jupiter_crm.py -c jupiter_crm3d_F100_nu0.1.yaml --output-dir ./260602_crm3d_F100_nu0.1 &> log.260602_crm3d_F100_nu0.1 &
 
 # dungeon2
+#DEVICE_ID=1 python -u run_jupiter_crm.py -c jupiter_crm3d_F100_nu0.01.yaml --output-dir ./260602_crm3d_F100_nu0.01 &> log.260602_crm3d_F100_nu0.01 &
+torchrun --nproc-per-node 2 run_jupiter_crm.py -c jupiter_crm3d_F100_nu0.01.yaml --output-dir ./260602_crm3d_F100_nu0.01 &> log.260602_crm3d_F100_nu0.01 &
+
 #DEVICE_ID=0 python -u run_jupiter_crm.py -r jupiter_crm_100.final.restart -c jupiter_crm_100.yaml --output-dir ./260520_crm_100 &> log.260520_crm_100 &
 #DEVICE_ID=1 python -u run_jupiter_crm.py -r jupiter_crm_7.5.final.restart -c jupiter_crm_7.5.yaml --output-dir ./260520_crm_7.5 &> log.260520_crm_7.5 &
 #DEVICE_ID=1 python -u run_jupiter_dry.py -r jupiter_dry_100.final.restart -c jupiter_dry_100.yaml --output-dir ./260520_dry_100 &> log.260520_dry_100 &
 
 # dungeon3
+torchrun --nproc-per-node 2 run_jupiter_crm.py -c jupiter_crm3d_F100_nu1.0.yaml --output-dir ./260602_crm3d_F100_nu1.0 &> log.260602_crm3d_F100_nu1.0 &
 #DEVICE_ID=0 python -u run_jupiter_crm.py -r jupiter_crm_1000.final.restart -c jupiter_crm_1000.yaml --output-dir ./260520_crm_1000 &> log.260520_crm_1000 &
 #DEVICE_ID=1 python -u run_jupiter_dry.py -r jupiter_dry_1000.final.restart -c jupiter_dry_1000.yaml --output-dir ./260520_dry_1000 &> log.260520_dry_1000 &
