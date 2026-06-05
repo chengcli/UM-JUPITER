85dd4fd388f192dd5612fa7e28ae37ac41ace196
Cheng Li <chengcli@dart9>
2026-06-05 00:00:51 -0400

    continue dart9

--- run_jupiter_crm2d.sh
+++ run_jupiter_crm2d.sh
@@ -5,7 +5,7 @@ EXP_ROOT=${HOME}/data/2026.JupiterCRM/
 export HDF5_USE_FILE_LOCKING=FALSE
 
 # case1
-EXP_NAME=crm2d_H2O-NH3_F10_nu100.0
+EXP_NAME=crm2d_H2O-NH3_F10_nu1.0
 EXP_INP=jup_${EXP_NAME}.yaml
 EXP_DIR=jup_${EXP_NAME}
     #-r jup_${EXP_NAME}.final.restart \
@@ -13,7 +13,7 @@ MASTER_PORT=29500 DEVICE_ID=0 python -u run_jupiter_moist.py \
     -c ${EXP_INP} --output-dir ${EXP_ROOT}/${EXP_DIR} &>> ${EXP_ROOT}/log.${EXP_DIR} &
 
 # case2
-EXP_NAME=crm2d_H2O-NH3_F10_nu1000.0
+EXP_NAME=crm2d_H2O-NH3_F10_nu10.0
 EXP_INP=jup_${EXP_NAME}.yaml
 EXP_DIR=jup_${EXP_NAME}
     #-r jup_${EXP_NAME}.final.restart \
