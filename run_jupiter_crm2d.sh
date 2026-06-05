53d8baf24b4462d1fdd485fca350b474435ca5a5
Cheng Li <cli@dart3>
2026-06-04 23:21:04 -0400

    continue dart3

--- run_jupiter_crm2d.sh
+++ run_jupiter_crm2d.sh
@@ -6,13 +6,17 @@ EXP_ROOT=${HOME}/data/2026.JupiterCRM/
 export HDF5_USE_FILE_LOCKING=FALSE
 
 # case1
-EXP_NAME=crm2d_H2O-NH3_F100_nu100.0
+EXP_NAME=crm2d_H2O-NH3_F100_nu1.0
 EXP_INP=jup_${EXP_NAME}.yaml
-EXP_DIR=${DATE_STR}_${EXP_NAME}
-MASTER_PORT=29500 DEVICE_ID=0 python -u run_jupiter_moist.py -c ${EXP_INP} --output-dir ${EXP_ROOT}/${EXP_DIR} &> ${EXP_ROOT}/log.${EXP_DIR} &
+EXP_DIR=jup_${EXP_NAME}
+MASTER_PORT=29500 DEVICE_ID=0 python -u run_jupiter_moist.py \
+    -r jup_${EXP_NAME}.final.restart \
+    -c ${EXP_INP} --output-dir ${EXP_ROOT}/${EXP_DIR} &>> ${EXP_ROOT}/log.${EXP_DIR} &
 
 # case2
-EXP_NAME=crm2d_H2O-NH3_F100_nu1000.0
+EXP_NAME=crm2d_H2O-NH3_F100_nu10.0
 EXP_INP=jup_${EXP_NAME}.yaml
-EXP_DIR=${DATE_STR}_${EXP_NAME}
-DEVICE_ID=1 python -u run_jupiter_moist.py -c ${EXP_INP} --output-dir ${EXP_ROOT}/${EXP_DIR} &> ${EXP_ROOT}/log.${EXP_DIR} &
+EXP_DIR=jup_${EXP_NAME}
+DEVICE_ID=1 python -u run_jupiter_moist.py \
+    -r jup_${EXP_NAME}.final.restart \
+    -c ${EXP_INP} --output-dir ${EXP_ROOT}/${EXP_DIR} &>> ${EXP_ROOT}/log.${EXP_DIR} &
